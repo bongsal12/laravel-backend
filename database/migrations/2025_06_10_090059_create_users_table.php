209656d9b076b548856cdc7e641d7c3d5a2348cd
bongsal12 <nhunpisal11@gmail.com>
2025-06-19 14:11:59 +0700

    fix: database duplicate column

--- database/migrations/2025_06_10_090059_create_users_table.php
+++ database/migrations/2025_06_10_090059_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('password');
+            $table->string('email');
             $table->string('password');
             $table->timestamps();
         });
