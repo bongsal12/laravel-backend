aa4b922c117c96fda6b6f0d55121ceff1c269792
bongsal12 <nhunpisal11@gmail.com>
2025-06-19 17:52:57 +0700

    no v1

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,11 @@
 
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\MovieController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::group(['prefix' => 'v1'], function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::resource('genres', GenreController::class);
-    Route::resource('movies', MovieController::class);
-});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::resource('genres', GenreController::class);
+Route::resource('movies', MovieController::class);
